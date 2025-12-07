64570159bf10d8bc02973c59b1f5adbc7c3270ee
IrinaB <balkarovairina@gmail.com>
2025-12-07 20:38:37 +0100

    Add auth form

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,18 @@
+'use client'
+
 import React from 'react'
+import { SignUpSchema } from '@/lib/validations'
+import AuthForm from '@/components/forms/AuthForm'
 
 const SignUp = () => {
-    return <div></div>
+    return (
+        <AuthForm
+            formType="SIGN_UP"
+            schema={SignUpSchema}
+            defaultValues={{ email: '', password: '', name: '', username: '' }}
+            onSubmit={(data: any) => Promise.resolve({ success: true, data })}
+        />
+    )
 }
 
 export default SignUp
